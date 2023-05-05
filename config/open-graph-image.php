ef3c984cbed950febfd1c8f5497297b4ffd5bd8a
NinjonikSVK <petricekzatko@gmail.com>
2023-05-05 17:02:50 +0200

    - og tags test

---
+++ config/open-graph-image.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'image' => [
+        'extension' => 'jpg',
+        'quality' => 100,
+        'width' => 1200,
+        'height' => 630,
+    ],
+
+    // The cache location to use.
+    'storage' => [
+        'disk' => 'public',
+        'path' => 'social/open-graph',
+    ],
+
+    // Whether to use the browse URL instead of the HTML input.
+    // This is slower, but makes fonts available.
+    // Alternative: http
+    'method' => 'html',
+
+    'metatags' => [
+        'og:title' => 'title',
+        'og:description' => 'description',
+        'og:type' => 'type',
+        'og:url' => 'url',
+    ],
+];
