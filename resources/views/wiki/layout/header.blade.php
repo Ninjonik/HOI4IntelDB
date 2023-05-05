7c26c7d77adf5103adbd5d29b57bb86253da093e
NinjonikSVK <petricekzatko@gmail.com>
2023-05-05 17:20:55 +0200

    Revert "- og tags test"
    This reverts commit badef1cbd3b0f36e090f274bffa6e80cf76b621e.

--- resources/views/wiki/layout/header.blade.php
+++ resources/views/wiki/layout/header.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{ url('/themes/wiki/css/main.css') }}" />
     <script src="{{ url('/themes/wiki/js/uikit.js') }}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <x-open-graph-image::metatags/>
+    <x-open-graph-image::metatags title="Vormkracht10" subtitle="Slimme websites" />
 </head>
 
 <body>
