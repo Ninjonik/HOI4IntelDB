22e3bf2c18c7d7c3d195fed918cb45a2caa0c062
NinjonikSVK <petricekzatko@gmail.com>
2023-05-08 14:20:39 +0200

    - better steam login

--- resources/views/pre_steam_api.blade.php
+++ resources/views/pre_steam_api.blade.php
@@ -23,6 +23,21 @@
     <link rel="stylesheet" href="{{ url('/themes/steam/css/plugins.css') }}">
     <link rel="stylesheet" href="{{ url('/themes/steam/css/main.css') }}">
 
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://hoi.theorganization.eu/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="HOI4Intel - The Revolutionary HOI4 Discord Bot">
+    <meta property="og:description" content="Steam Verification ">
+    <meta property="og:image" content="{{ url('/themes/steam/img/hoi4intel_steam.png') }}">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="hoi.theorganization.eu">
+    <meta property="twitter:url" content="https://hoi.theorganization.eu/">
+    <meta name="twitter:title" content="HOI4Intel - The Revolutionary HOI4 Discord Bot">
+    <meta name="twitter:description" content="Steam Verification ">
+    <meta name="twitter:image" content="{{ url('/themes/steam/img/hoi4intel_steam.png') }}">
+
 </head>
 
 
