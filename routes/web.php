0632175c6876f3cc68cb8f33d3e9d6c3754c250e
NinjonikSVK <petricekzatko@gmail.com>
2023-05-19 16:54:44 +0200

    - removed unnecessary line

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use App\Http\Livewire\WikiCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use GuzzleHttp\Client;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
