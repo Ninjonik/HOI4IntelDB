21991ae5e09e16e691944dabf864dd7b531195bc
NinjonikSVK <petricekzatko@gmail.com>
2023-06-28 16:50:54 +0200

    - added tawk.to

--- resources/views/layouts/status.blade.php
+++ resources/views/layouts/status.blade.php
@@ -115,7 +115,7 @@
     (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/649c3fffcc26a871b0252cf6/1h415k056';
+        s1.src='{{ env("TAWKTO_EMBED_URL") }}';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
