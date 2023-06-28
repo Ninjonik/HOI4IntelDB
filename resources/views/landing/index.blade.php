9d5cde13843fe9a8f4caf4319a03d5f55b089cea
NinjonikSVK <petricekzatko@gmail.com>
2023-06-28 16:28:39 +0200

    - added tawk.to

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -706,6 +706,20 @@
 <!--====== Main js ======-->
 <script src="{{ url('/themes/landing/js/main.js') }}"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/649c3fffcc26a871b0252cf6/1h415k056';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
