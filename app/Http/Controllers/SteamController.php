aab1fbeda3072bbb1c8e207aefa9894b1785529b
NinjonikSVK <petricekzatko@gmail.com>
2023-06-30 15:04:22 +0200

    - steam fix

--- app/Http/Controllers/SteamController.php
+++ app/Http/Controllers/SteamController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Players;
 use Illuminate\Http\Request;
 use Auth;
@@ -11,11 +13,10 @@ use function PHPUnit\Framework\isEmpty;
 
 class SteamController extends Controller
 {
-
     public function init($id)
     {
-        return view("pre_steam_api", ["data"=>$id]);
         Session::put('discord_id', $id);
+        return view("pre_steam_api", ["data" => $id]);
     }
 
     public function redirect()
@@ -26,29 +27,28 @@ class SteamController extends Controller
     public function callback()
     {
         try {
-
             $user = Socialite::driver('steam')->user();
             $discord_id = intval(Session::get('discord_id'));
 
             $steam_url = $user->user["profileurl"];
 
-            $url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=".env("STEAM_CLIENT_SECRET")."&steamid=".$user->id."&format=json";
+            $url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=" . env("STEAM_CLIENT_SECRET") . "&steamid=" . $user->id . "&format=json";
             $json = file_get_contents($url);
             $json = json_decode($json);
             $gamesData = $json->response->games;
 
-            $hoi = false;
+            $hasHoi4 = false;
 
-            foreach($gamesData as $gameData){
+            foreach ($gamesData as $gameData) {
                 if ($gameData->appid == 394360) {
-                    $hoi = true;
+                    $hasHoi4 = true;
+                    break;
                 }
             }
 
-            if ($hoi){
-                print($discord_id);
+            if ($hasHoi4) {
                 $player = Players::where('discord_id', $discord_id)->first();
-                if(empty($player->rating)){
+                if (empty($player->rating)) {
                     try {
                         $new_player = new Players();
                         $new_player->steam_id = $user->id;
@@ -56,18 +56,18 @@ class SteamController extends Controller
                         $new_player->rating = 0.5;
                         $new_player->profile_link = $steam_url;
                         $new_player->save();
+
                         $status = "Success!";
                         $description = "Your steam account has been successfully linked with your new discord account. Please run /verify command again on the discord server.";
                     } catch (Exception $e) {
                         $status = "There has been an error...";
-                        $description = "There has been an error with validating your steam account. This is caused by
-                        your steam account already being linked with another discord account.
-                        Once you resolve this issue you can try again. Feel free to Contact HOI4Intel's Staff.";
+                        $description = "There has been an error with validating your steam account. This is caused by your steam account already being linked with another discord account. Once you resolve this issue you can try again. Feel free to Contact HOI4Intel's Staff.";
                     }
                 } else {
                     try {
                         Players::where('discord_id', $discord_id)
                             ->update(['steam_id' => $user->id, 'profile_link' => $steam_url]);
+
                         $status = "Success!";
                         $description = "Your steam account has been successfully linked with your discord account. Please run /verify command again on the discord server.";
                     } catch (Exception $e) {
@@ -77,31 +77,23 @@ class SteamController extends Controller
                 }
             } else {
                 $status = "There has been an error...";
-                $description = "There has been an error with validating your steam account. This may be caused by either
-                Private Profile or by you not having Hearts of Iron IV bought on your steam account.
-                Or this steam account has already been linked to other discord account.
-                Once you resolve these issues you can try again. Feel free to Contact HOI4Intel's Staff.";
+                $description = "There has been an error with validating your steam account. This may be caused by either Private Profile or by you not having Hearts of Iron IV bought on your steam account. Or this steam account has already been linked to another discord account. Once you resolve these issues, you can try again. Feel free to Contact HOI4Intel's Staff.";
             }
 
             $data = [
                 "status" => $status,
                 "description" => $description
             ];
-            return view("steam_api", ["data"=>$data]);
-
-
+            return view("steam_api", ["data" => $data]);
         } catch (Exception $e) {
             $status = "There has been an error...";
-            $description = "There has been an error with validating your steam account. This may be caused by either
-                Private Profile or by you not having Hearts of Iron IV bought on your steam account.
-                Or this steam account has already been linked to other discord account.
-                Once you resolve these issues you can try again. Feel free to Contact HOI4Intel's Staff.";
+            $description = "There has been an error with validating your steam account. This may be caused by either Private Profile or by you not having Hearts of Iron IV bought on your steam account. How to: https://asapguide.com/how-to-make-steam-profile-public/ - once you resolve these issues, you can try again. Feel free to Contact HOI4Intel's Staff.";
+
             $data = [
                 "status" => $status,
                 "description" => $description
             ];
-            return view("steam_api", ["data"=>$data]);
-            // dd($e->getMessage());
+            return view("steam_api", ["data" => $data]);
         }
     }
 }
