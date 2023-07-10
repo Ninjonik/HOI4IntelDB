3130f9ef6b122b241cb6fa1a2d65d5ace16f5ef8
NinjonikSVK <petricekzatko@gmail.com>
2023-07-10 21:26:14 +0200

    Server System - added custom channels functionality - added verify role functionality

--- app/Http/Livewire/GuildSettings.php
+++ app/Http/Livewire/GuildSettings.php
@@ -15,12 +15,18 @@ class GuildSettings extends Component
     public $steam_verification;
     public $data;
     public $log_channel;
+    public $custom_channel;
+    public $custom_channel_2;
+    public $verify_role;
 
     protected $rules = [
         'guild_name' => 'required|min:6',
         'guild_desc' => '',
         'steam_verification' => 'required',
         'log_channel' => '',
+        'custom_channel' => '',
+        'custom_channel_2' => '',
+        'verify_role' => '',
     ];
 
     public function submit()
@@ -32,6 +38,9 @@ class GuildSettings extends Component
         $data->guild_desc = $this->guild_desc;
         $data->steam_verification = $this->steam_verification;
         $data->log_channel = $this->log_channel;
+        $data->custom_channel = $this->custom_channel;
+        $data->custom_channel_2 = $this->custom_channel_2;
+        $data->verify_role = $this->verify_role;
         $data->save();
 
         $client = new Client();
@@ -68,11 +77,13 @@ class GuildSettings extends Component
         $this->guild_desc = $this->data->guild_desc;
         $this->steam_verification = $this->data->steam_verification;
         $this->log_channel = str($this->data->log_channel);
+        $this->custom_channel = str($this->data->custom_channel);
+        $this->custom_channel_2 = str($this->data->custom_channel_2);
+        $this->verify_role = str($this->data->verify_role);
     }
 
     public function render()
     {
-        // Fetch channel data from the API
         $client = new Client();
         $response = $client->get(env('COMMS_URL') . '/get/guild/channels', [
             'headers' => [
@@ -81,6 +92,7 @@ class GuildSettings extends Component
             'json' => [
                 'token' => env("COMMS_TOKEN"),
                 'guild_id' => $this->data->guild_id,
+                'voice' => false
             ],
         ]);
         if ($response->getStatusCode() === 200) {
@@ -89,7 +101,41 @@ class GuildSettings extends Component
         } else {
             $channels = [];
         }
-        return view('livewire.guild-settings', ['data' => $this->data, 'channels' => $channels])->layout('livewire.layouts.base');
+
+        $voice_response = $client->get(env('COMMS_URL') . '/get/guild/channels', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'token' => env("COMMS_TOKEN"),
+                'guild_id' => $this->data->guild_id,
+                'voice' => true
+            ],
+        ]);
+        if ($voice_response->getStatusCode() === 200) {
+            $voice_channelData = json_decode($voice_response->getBody(), true);
+            $voice_channels = $voice_channelData;
+        } else {
+            $voice_channels = [];
+        }
+
+        $roles_response = $client->get(env('COMMS_URL') . '/get/guild/roles', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'token' => env("COMMS_TOKEN"),
+                'guild_id' => $this->data->guild_id
+            ],
+        ]);
+        if ($roles_response->getStatusCode() === 200) {
+            $roles_channelData = json_decode($roles_response->getBody(), true);
+            $roles = $roles_channelData;
+        } else {
+            $roles = [];
+        }
+
+        return view('livewire.guild-settings', ['data' => $this->data, 'channels' => $channels, 'voice_channels' => $voice_channels, 'roles' => $roles])->layout('livewire.layouts.base');
     }
 
 }
--- resources/views/livewire/guild-settings.blade.php
+++ resources/views/livewire/guild-settings.blade.php
@@ -6,21 +6,40 @@
 
         <form wire:submit.prevent="submit">
             <div class="card-body">
+                <h2>Basic Settings</h2>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Name</label>
                     <input type="text" wire:model="guild_name" class="form-control">
                     @error('guild_name') <span class="error">{{ $message }}</span> @enderror
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Description</label>
+                    <label for="exampleInputPassword1">Description (optional)</label>
                     <input type="text" wire:model="guild_desc" class="form-control">
                     @error('guild_desc') <span class="error">{{ $message }}</span> @enderror
                 </div>
+                <h2>Steam Verification (optional)</h2>
                 <div class="form-group">
-                    <label for="exampleInputPassword1"><b class="text-primary">Steam Verification</b> required for reserving countries</label>
                     <input type="checkbox" name="steam_verification" true-value="1" false-value="0" wire:model="steam_verification">
+                    <label for="steam_verification"><b class="text-primary">Steam Verification</b> required for reserving countries</label>
                     @error('steam_verification') <span class="error">{{ $message }}</span> @enderror
                 </div>
+                @if($steam_verification)
+                    <div class="form-group" wire:ignore>
+                        <label for="verify_role" class="col-3">Role that user gets after being verified</label>
+                        <select class="select2" data-placeholder="Select a State" style="width: 100%;" id="verify_role" wire:model="verify_role">
+                            @foreach($roles as $role)
+                                <option value="{{ $role['role_id'] }}">
+                                    {{ $role['role_name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+
+                        @error("verify_role")
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                @endif
+                <h2>Custom Channels (optional)</h2>
                 <div class="form-group" wire:ignore>
                     <label for="log_channel" class="col-3">Logs Channel</label>
                     <select class="select2" data-placeholder="Select a State" style="width: 100%;" id="log_channel" wire:model="log_channel">
@@ -32,6 +51,34 @@
                     </select>
 
                     @error("log_channel")
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-group" wire:ignore>
+                    <label for="custom_channel" class="col-3">Channel for creating temporary custom channels</label>
+                    <select class="select2" data-placeholder="Select a State" style="width: 100%;" id="custom_channel" wire:model="custom_channel">
+                        @foreach($voice_channels as $channel)
+                            <option value="{{ $channel['channel_id'] }}">
+                                {{ $channel['channel_name'] }}
+                            </option>
+                        @endforeach
+                    </select>
+
+                    @error("custom_channel")
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-group" wire:ignore>
+                    <label for="custom_channel_2" class="col-3">Channel for creating permanent custom channels</label>
+                    <select class="select2" data-placeholder="Select a State" style="width: 100%;" id="custom_channel_2" wire:model="custom_channel_2">
+                        @foreach($voice_channels as $channel)
+                            <option value="{{ $channel['channel_id'] }}">
+                                {{ $channel['channel_name'] }}
+                            </option>
+                        @endforeach
+                    </select>
+
+                    @error("custom_channel_2")
                     <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
@@ -54,6 +101,18 @@
         // Manually trigger Livewire update when select2 value changes
         $('#log_channel').on('change', function() {
             @this.set('log_channel', $(this).val());
+        });
+
+        $('#custom_channel').on('change', function() {
+            @this.set('custom_channel', $(this).val());
+        });
+
+        $('#custom_channel_2').on('change', function() {
+            @this.set('custom_channel_2', $(this).val());
+        });
+
+        $('#verify_role').on('change', function() {
+            @this.set('custom_channel_2', $(this).val());
         });
     });
 </script>
