eac62fc9de97bc373ce03ea9f3f71434881fd7ec
NinjonikSVK <petricekzatko@gmail.com>
2023-09-12 18:12:49 +0200

    QoL

--- app/Http/Livewire/PlayersIndex.php
+++ app/Http/Livewire/PlayersIndex.php
@@ -21,8 +21,6 @@ class PlayersIndex extends Component
     protected $paginationTheme = 'bootstrap';
     public $search = '';
 
-    // RENDER
-
     public function render()
     {
         $data = Players::when($this->search, function ($query, $search) {
